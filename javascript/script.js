80bbf36a7fac251d96f65b3e6f829b8e12af5f93
ida hjortnæs <idahjortnaes@gmail.com>
2023-01-05 14:47:41 +0100

    mobil

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,35 @@
-const buttons = document.querySelectorAll("[data-carousel-button]");
+let slideIndex = 1;
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+showSlides(slideIndex);
 
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
-  });
-});
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let sildes = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > sildes.length) {
+    slideIndex = 1;
+  }
+  if (n < i) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  dots[slideIndex - 1].className += " active";
+}
