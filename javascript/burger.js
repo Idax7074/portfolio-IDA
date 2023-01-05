dfcce399baa0edb335f6d1cb27525ad8a145f0b5
ida hjortnæs <idahjortnaes@gmail.com>
2023-01-05 12:12:17 +0100

    slideshow

---
+++ javascript/burger.js
@@ -0,0 +1,28 @@
+// Lav variablen "btn", der henviser til ".toggle-btn"
+const btn = document.querySelector(".toggle-btn");
+
+// Lav variablen "menu", der henviser til ".main-menu"
+const menu = document.querySelector(".main-menu");
+
+// Lav funktionen "toggleMenu()" med følgende funktionalitet
+function toggleMenu() {
+  // toggle klassen "shown" på menu vha. classList.toggle
+  menu.classList.toggle("shown");
+
+  // lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
+  const menuShown = menu.classList.contains("shown");
+
+  // spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+  if (menuShown) {
+    console.log(menuShown);
+  } else {
+    console.log(menuShown);
+  }
+}
+
+// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+btn.addEventListener("click", toggleMenu);
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
